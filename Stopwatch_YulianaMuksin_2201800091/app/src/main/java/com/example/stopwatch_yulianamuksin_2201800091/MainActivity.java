e5587e57ea6c1c653e0fbbd8f6e98134dc45c1ac
Yuliana <yuliana.int@fabelio.com>
2020-10-06 21:56:46 +0700

    initial commit

---
+++ Stopwatch_YulianaMuksin_2201800091/app/src/main/java/com/example/stopwatch_yulianamuksin_2201800091/MainActivity.java
@@ -0,0 +1,90 @@
+// NAMA: YULIANA MUKSIN
+// NIM: 2201800091
+// KELAS: LM01
+
+package com.example.stopwatch_yulianamuksin_2201800091;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class MainActivity extends Activity {
+    private int seconds = 0;
+    private boolean running;
+    private boolean wasRunning;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState != null) {
+            seconds = savedInstanceState.getInt("seconds");
+            running = savedInstanceState.getBoolean("running");
+            wasRunning = savedInstanceState.getBoolean("wasRunning");
+        }
+        runTimer();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (wasRunning) {
+            running = true;
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+
+    // Start the stopwatch running when user clicks Start button
+    public void onClickStart(View view) {
+        running = true;
+    }
+
+    // Stop the stopwatch running when user clicks Stop button
+    public void onClickStop(View view) {
+        running = false;
+    }
+
+    // Reset the stopwatch when user clicks Reset button
+    public void onClickReset(View view) {
+        running = false;
+        seconds = 0;
+    }
+
+    // Sets the number of seconds on the timeView
+    private void runTimer() {
+        final TextView timeView = (TextView)findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
+                String time = String.format(Locale.getDefault(),
+                        "%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if (running) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
+    }
+}
