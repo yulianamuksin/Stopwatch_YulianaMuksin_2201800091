7b16d597795a098a63d3ed44957de742e7d54c15
Yuliana <yuliana.int@fabelio.com>
2020-10-30 03:49:54 +0700

    Yuliana Muksin 2201800091 Android Chapter 14

---
+++ 2201800091_Chapter14/app/src/main/java/com/example/YulianaMuksin_2201800091_CatChat/HelpActivity.java
@@ -0,0 +1,22 @@
+package com.example.YulianaMuksin_2201800091_CatChat;
+
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class HelpActivity extends AppCompatActivity {
+    private Toolbar supportActionBar;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
---
+++ 2201800091_Chapter14/app/src/main/java/com/example/YulianaMuksin_2201800091_CatChat/SentItemsFragment.java
@@ -0,0 +1,17 @@
+package com.example.YulianaMuksin_2201800091_CatChat;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SentItemsFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_sent_items, container, false);
+    }
+}
